c41e69ab6d14ceaf7eb2f0c5bd0337141964ca3a
iyibrahim <>
2021-05-29 15:31:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Write your code below this line 👇
+print("Hello world!")
+#parenthesis make it into a string
+print("Hello world!\nHello world!")
+print("Hello" + " " + "David")
+#space print gives an indentation error
+print('Hello ' + input("what is your name?"))
+# add # by pressing Ctrl / or Ctrl Z to undo
+print( len( input("what is your name?")))
+name = input("what is your name?")
+length = len(name)
+print(length)
