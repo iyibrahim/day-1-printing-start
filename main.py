d38c5f1975cce1d63ef055f5577303737c2c07b5
iyibrahim <>
2021-05-29 16:16:08 +0000

    Day 4

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# Write your code below this line 👇
-print("Hello world!")
-#parenthesis make it into a string
-print("Hello world!\nHello world!")
-print("Hello" + " " + "David")
-#space print gives an indentation error
-print('Hello ' + input("what is your name?"))
-# add # by pressing Ctrl / or Ctrl Z to undo
-print( len( input("what is your name?")))
-name = input("what is your name?")
-length = len(name)
-print(length)
+import random
+
+random_integer =random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random()
+print(random_float)
